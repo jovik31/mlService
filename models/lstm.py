fa37dc1d73a2c3cffba998cd88afdd35a469c5c7
jovik31 <joaogvicente@gmail.com>
2023-12-03 20:41:55 +0000

    Create write feature for mongo db

--- models/lstm.py
+++ models/lstm.py
@@ -1,8 +1,40 @@
+
+from keras.src.layers.attention.multi_head_attention import activation
 import tensorflow as tf
-from keras.layers import LSTM
 import keras
+import json
 
 
-class model(keras.Model):
+def decodeJSON(file_name):
 
-    def__init__()
+    return json.load(file_name)
+
+
+class LSTM_Model(keras.Model):
+
+    def __init__(self, file_name):
+
+        super(LSTM_Model, self).__init__()
+        parameters = decodeJSON(file_name)
+        self.lstm = keras.layers.LSTM(
+            parameters['lstm_neurons'], activation=parameters['lstm_activation'])
+        self.bn = keras.layers.BatchNormalization()
+
+        if "droupout_rate" in parameters:
+            self.dp1 = keras.layers.Dropout(parameters['droupout_rate'])
+            self.dp2 = keras.layers.Dropout(parameters['droupout_rate'])
+            self.dp3 = keras.layers.Dropout(parameters['droupout_rate'])
+
+        for layer in parameters['dense_layers']:
+
+            self.dense1 = keras.layers.Dense(
+                layer['neurons'], activation=layer['activation'])
+            self.bn1 = keras.layers.BatchNormalization()
+
+            self.dense2 = keras.layers.Dense(
+                layer['neurons'], activation=layer['activation'])
+            self.bn2 = keras.layers.Dense(
+                layer['neurons'], activation=layer['activation'])
+
+        self.out = keras.layers.Dense(
+            parameters['output_size'], activation='sigmoid')
